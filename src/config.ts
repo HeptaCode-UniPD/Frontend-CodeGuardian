9c4d50e5e43e2c6a0d5cd6b875f5283105276781
angela <angela.canazza@studenti.unipd.it>
2026-04-01 22:32:35 +0200

    modificato il path iniziale per iser service

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL_USER = import.meta.env.URL_USER_SERVICE || "http://localhost:3000";
+export const API_BASE_URL_USER = import.meta.env.VITE_URL_USER_SERVICE || "http://userservice.codeguardian.local:3000";
