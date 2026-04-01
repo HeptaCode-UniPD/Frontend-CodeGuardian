e4a4eb0d1169344757255efdb0a20a7a33ca17b4
angela <angela.canazza@studenti.unipd.it>
2026-04-01 23:36:37 +0200

    cambiato l'ip del backend (userservice)

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL_USER = import.meta.env.VITE_URL_USER_SERVICE || "http://51.21.152.139:3000";
+export const API_BASE_URL_USER = import.meta.env.VITE_URL_USER_SERVICE || "http://16.16.194.41:3000";
