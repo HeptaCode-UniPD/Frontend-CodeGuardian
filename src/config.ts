e6e6c5aa718475154e3e6cf0bd01246c62abad2b
angela <angela.canazza@studenti.unipd.it>
2026-04-03 12:13:30 +0200

    errore nel config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL_USER = import.meta.env.VITE_URL_USER_SERVICE || "http://3.65.40.23:3000";
-export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_USER_SERVICE || "http://3.75.177.10:4000";
+export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_ANALYSIS || "http://3.75.177.10:4000";
