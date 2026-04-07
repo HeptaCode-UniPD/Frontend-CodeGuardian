dbc1cfe801c2a41d7267edcb49caa097c074073e
angela <angela.canazza@studenti.unipd.it>
2026-04-07 17:11:43 +0200

    ip backend cambiato

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL_USER = import.meta.env.VITE_URL_USER_SERVICE || "http://3.65.40.23:3000";
-export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_ANALYSIS || "http://3.121.200.58:4000";
+export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_ANALYSIS || "http://35.159.79.7.58:4000";
