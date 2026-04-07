bd5246d54cd70cc6712020bda8fa35d105dcf712
angela <angela.canazza@studenti.unipd.it>
2026-04-07 18:37:18 +0200

    aggiornato ip ms

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL_USER = import.meta.env.VITE_URL_USER_SERVICE || "http://3.65.40.23:3000";
-export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_ANALYSIS || "http://51.102.113.131:4000";
+export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_ANALYSIS || "http://3.79.255.187:4000";
