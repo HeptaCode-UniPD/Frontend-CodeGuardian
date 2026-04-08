b7d3255a2e4c134fa7db16248d2252c0ec6c5756
angela <angela.canazza@studenti.unipd.it>
2026-04-08 03:07:39 +0200

    aggiornato con api gateway

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL_USER = import.meta.env.VITE_URL_USER_SERVICE || "http://3.65.40.23:3000";
-export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_ANALYSIS || "http://63.178.195.4:4000";
+export const API_BASE_URL_USER = import.meta.env.VITE_URL_USER_SERVICE || "https://51fehz5pi5.execute-api.eu-central-1.amazonaws.com/prod";
+export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_ANALYSIS || "https://zx8fxs5l93.execute-api.eu-central-1.amazonaws.com/prod";
