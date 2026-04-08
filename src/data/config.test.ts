1f514ba4ff3494108f6b024920e676cdef8735b6
angela <angela.canazza@studenti.unipd.it>
2026-04-08 21:27:58 +0200

    corretto test

--- src/data/config.test.ts
+++ src/data/config.test.ts
@@ -12,7 +12,7 @@ describe('config', () => {
     const { API_BASE_URL_USER, API_BASE_URL_ANALYSIS } = await import('./config');
 
     expect(API_BASE_URL_USER).toBe('https://51fehz5pi5.execute-api.eu-central-1.amazonaws.com/prod');
-    expect(API_BASE_URL_ANALYSIS).toBe('https://zx8fxs5l93.execute-api.eu-central-1.amazonaws.com/prod');
+    expect(API_BASE_URL_ANALYSIS).toBe('https://tqp87woa5a.execute-api.eu-central-1.amazonaws.com/prod');
   });
 
   it('usa le variabili d\'ambiente se definite', async () => {
