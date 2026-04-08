50ea47009ab92ac2d91815b7410e4605e68ad5b9
angela <angela.canazza@studenti.unipd.it>
2026-04-08 21:24:54 +0200

    cambiata api

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL_USER = import.meta.env.VITE_URL_USER_SERVICE || "https://51fehz5pi5.execute-api.eu-central-1.amazonaws.com/prod";
-export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_ANALYSIS || "https://zx8fxs5l93.execute-api.eu-central-1.amazonaws.com/prod";
+export const API_BASE_URL_ANALYSIS = import.meta.env.VITE_URL_ANALYSIS || "https://tqp87woa5a.execute-api.eu-central-1.amazonaws.com/prod";
